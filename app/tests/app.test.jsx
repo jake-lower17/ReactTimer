5551f996832fe152459d2d8ecc7d4873d4ac2ef7
Jacob Lower <jake.lower17@gmail.com>
2017-01-24 12:13:25 -0600

    Added Testing

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,13 @@
+var expect = require('expect');
+
+describe('First Test', () => {
+  it('Should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe('Second Test', () => {
+  it('Should properly run tests', () => {
+    expect(2).toBe(2);
+  });
+});

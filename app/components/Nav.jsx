945405cfc118b6b0cd4307e475837fc16c6bf56b
Procyon <Procyon>
2017-01-23 21:20:56 -0600

    First Commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Nav = React.createClass({
+
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link">
+                Timer
+              </IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active-link">
+                Countdown
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li className="menu-text">
+                Created by Jacob Lower
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  },
+});
+
+module.exports = Nav;
